08afe308eacf929c1004571a2be07469dc173d78
riptorn01@gmail.com <Trinity1>
2016-11-26 14:35:26 -0600

    Rev2

--- Final_Project/FinalProject.c
+++ Final_Project/FinalProject.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #define STRSIZ 6
-#define ROOM_DESC 300
+#define ROOM_DESC 1000
 
 typedef enum {
-	HealthPotion, TableLeg, GlassBottle
+	HealthPotion, TableLeg, GlassBottle, AdrenalShot, PaperClip
 } ObjectType;
 
 
@@ -14,44 +14,68 @@ typedef struct {
 } encounter_object;
 
 typedef struct {
+	char description[50];
+} help_commands;
+
+typedef struct {
 	encounter_object objects[10];
 	char dir[STRSIZ];
 	int seq;
 	char room_desc[ROOM_DESC];
-
+	help_commands commands[5];
+	int commandsCount;
 } room_template;
 
 
 
 int main()
 {
-	
+	jail_cell();
 
 	return 0;
 }
 
-char jail_cell()
+jail_cell()
 {
-
 	room_template encounter1;
 	encounter1.seq = 1;
-	strcpy("This is my first room description", encounter1.room_desc);
+	strcpy(encounter1.room_desc,"How do you want to escape the jail cell?\n");
 
-	encounter_object myEncounterObj;
-	strcpy("Ultra Health Potion", myEncounterObj.description);
-	myEncounterObj.type = HealthPotion;
-	encounter1.objects[0] = myEncounterObj;
+	encounter_object object1;
+	strcpy(object1.description,"Health Potion");
+	object1.type = HealthPotion;
+	encounter1.objects[0] = object1;
 
-	encounter_object myEncounterObj2;
-	strcpy("Health Potion", myEncounterObj2.description);
-	encounter1.objects[1] = myEncounterObj2;
+	encounter_object object2;
+	strcpy(object2.description,"Paper Clip");
+	object2.type = PaperClip;
+	encounter1.objects[1] = object2;
 
-	room_template encounter2;
-	strcpy("This is my 2nd room desc", encounter2.room_desc);
-	encounter2.seq = 2;
+	help_commands cmd;
+	strcpy(cmd.description, "South");
+	encounter1.commands[0] = cmd;
 
+	help_commands cmd1;
+	strcpy(cmd1.description, "Use Paperclip");
+	encounter1.commands[1] = cmd1;
 
+	encounter1.commandsCount = 2;
+	
+	
+	printf("%s", encounter1.room_desc);
+	char jail_input[14];
+	printf("What would you like to do?");
+	gets_s("%s", jail_input);
 
+	switch (switch_on)
+	{
+	default:
+		break;
+	}
+
+	
+
+	return 0;
 
 }
 
